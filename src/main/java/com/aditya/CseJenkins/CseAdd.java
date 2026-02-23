f66312976caebabf0c4eb79476d0a7a227822791
rialhasan <realhasan74@gmail.com>
2026-02-23 11:59:02 +0530

    commit

---
+++ src/main/java/com/aditya/CseJenkins/CseAdd.java
@@ -0,0 +1,11 @@
+package com.aditya.CseJenkins;
+
+public class CseAdd {
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int mul(int a, int b) {
+		return a * b;
+	}
+}
---
+++ src/test/java/com/aditya/CseJenkins/CseAddTest.java
@@ -0,0 +1,23 @@
+package com.aditya.CseJenkins;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CseAddTest {
+	
+	CseAdd c = new CseAdd();
+	
+	@Test
+	void csetest() {
+		int result = c.add(2, 3);
+		assertEquals(5, result);
+	}
+
+	@Test
+	void csetest1() {
+		int result = c.mul(2, 3);
+		assertEquals(6, result);
+	}
+
+}
